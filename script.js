5fef2bc29b6318df49449964ac5004151b3a6e2b
Philip Clark <ContactPhilipClark@protonmail.com>
2023-04-20 03:13:40 -0400

    added features

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const text = 'Howdy';
 const textElements = document.getElementsByClassName('text');
 const woods = document.getElementsByClassName('woodSelect');
-
+const fonts = document.getElementsByClassName('fontSelect');
 const input = document.getElementById('name');
+const backWidth = document.getElementById('backWidth');
+const fontColor = document.getElementById('color');
+
+const color = '#fff';
+const cameraAngle = { x: 1, y: 1 };
 
 [...woods].forEach((element) => {
   element.style = `background-image: url(${element.value})`;
   element.addEventListener('click', (button) => {
     document.getElementById('material').setAttribute('xlink:href', `${element.value}`);
-    [...stains].forEach((element) => {
-      element.style = `background: url('${element.value}')`;
-    });
+  });
+});
+
+[...fonts].forEach((element) => {
+  element.addEventListener('click', (button) => {
+    document.getElementById('words').setAttribute('font-family', button.target.value);
+    document.getElementById('wood').setAttribute('font-family', button.target.value);
+    console.log(button.target.value);
   });
 });
 
 input.addEventListener('change', (e) => {
   [...textElements].forEach((element) => {
-    element.innerHTML = `<tspan x="48%" dy="-1em">The</tspan>
+    element.innerHTML = `<tspan x="40%" dy="-1em">The</tspan>
     <tspan x="50%" dy="1.2em">${e.target.value}</tspan>
-    <tspan x="51%" dy="1.2em">Family</tspan>`;
+    <tspan x="60%" dy="1.2em">Family</tspan>`;
     console.log(e.target);
   });
+});
+
+backWidth.addEventListener('input', (e) => {
+  document.getElementById('wood').setAttribute('stroke-width', `${e.target.value / 10}em`);
 });
