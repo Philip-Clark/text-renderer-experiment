2e47be29623ea65350c13c3a604132eed06c7dcc
Philip Clark <ContactPhilipClark@protonmail.com>
2023-05-11 18:57:30 -0400

    feature: add wood select. add update on change

--- script.js
+++ script.js
@@ -2,21 +2,57 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const offset = document.getElementById('offset');
 const color = document.getElementById('color');
-const wood = document.getElementById('wood');
 const visualizer = document.getElementById('visualizer');
+const loadText = document.getElementById('loading');
+
+const woods = document.querySelectorAll('.woodSelect');
+
+woods.forEach((button) => {
+  button.style.background = `url(${button.value})`;
+  button.addEventListener('click', () => {
+    values['wood'] = button.value;
+    getSignRender();
+  });
+});
 
 const apiEndPoint = 'https://signrenderapi-production.up.railway.app/api/preview/';
+// const apiEndPoint = 'http://192.168.1.85:5000/api/preview';
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  let values = '?';
-  [text, offset, color, wood].forEach((input) => {
-    if (input.value === '') return;
-    values = values + `${input.id}=${input.value.replace('#', '')}&`;
+const values = {
+  text: 'Default',
+  offset: 20,
+  color: '#bf2020',
+  wood: 'https://th.bing.com/th/id/OIP.IHewWcp0eR9crdv9XwT_SQHaHa',
+};
+
+[text, offset, color].forEach((input) => {
+  input.addEventListener('change', () => {
+    values[input.id] = input.value;
+    getSignRender();
   });
+});
+const getSignRender = async () => {
+  loadText.style.opacity = 1;
 
-  const response = await fetch(apiEndPoint + values);
+  console.time('Response Time');
+
+  let query = '?';
+  for (const value in values) {
+    query += `&${value}=${values[value]}`;
+  }
+
+  console.log(query);
+
+  const response = await fetch(apiEndPoint + query.replace('#', ''));
   const jsonData = await response.json();
 
   visualizer.innerHTML = jsonData.sign;
+  console.timeEnd('Response Time');
+  loadText.style.opacity = 0;
+};
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  getSignRender();
 });
+
+getSignRender();
