c424494e19f2a1b61615dc59727e1ce0996bfa91
Philip Clark <ContactPhilipClark@protonmail.com>
2023-05-15 18:29:50 -0400

    add: backboard shapes, save button

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const color = document.getElementById('color');
 const visualizer = document.getElementById('visualizer');
 const loadText = document.getElementById('loading');
 const woods = document.querySelectorAll('.woodSelect');
+const backboardShapes = document.querySelectorAll('.backboardShape');
 const data = document.getElementById('data');
+const saveOutline = document.getElementById('saveOutline');
 
 const apiEndPoint = 'https://signrenderapi-production.up.railway.app/api/';
-// const apiEndPoint = 'http://192.168.1.85:5000/api/';
+// const apiEndPoint = 'http://127.0.0.1:5000/api/';
 
 woods.forEach((button) => {
   button.style.background = `url(${button.value})`;
@@ -16,6 +18,13 @@ woods.forEach((button) => {
     values['wood'] = button.value;
     const backboard = document.getElementById('texture').firstChild;
     backboard.setAttribute('xlink:href', values.wood);
+  });
+});
+
+backboardShapes.forEach((button) => {
+  button.addEventListener('click', () => {
+    values['backboardShape'] = button.value;
+    getSignRender();
   });
 });
 
@@ -60,6 +69,39 @@ const getSignRender = async () => {
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   getSignRender();
+});
+
+// Function to download data to a file
+// updated from : https://stackoverflow.com/a/30832210/17977603
+function download(data, filename, type) {
+  const file = new Blob([data], { type: type });
+  if (window.navigator.msSaveOrOpenBlob)
+    // IE10+
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  else {
+    // Others
+    var a = document.createElement('a'),
+      url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+}
+
+saveOutline.addEventListener('click', async (e) => {
+  e.preventDefault();
+  let query = '?';
+  for (const value in values) {
+    query += `&${value}=${values[value]}`;
+  }
+  const response = await fetch(apiEndPoint + 'outline/' + query.replace('#', ''));
+  const jsonData = await response.json();
+  download(jsonData.sign, `${values.text}-Family-Outline`, 'image/svg+xml');
 });
 
 getSignRender();
